83acf3557d4837eacff3ca3dbc6a1ee97a79ec85
toshyamsundar <toshyamsundar@yahoo.com>
2019-03-14 01:16:13 -0400

    Added comments & rearranged js, modified html for better reponsive layout

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //An array of default giphy categories
   var giphyCategories = [
     "Puppies",
     "Cats",
@@ -10,20 +11,19 @@ $(document).ready(function() {
     "Holidays",
     "Movies",
     "Music",
-    "Nature",
-    "Politics",
     "Science",
     "Sports",
     "Stickers",
-    "Batman",
-    "Ironman",
+    "DC Comics",
+    "Marvel Comics",
     "Cool"
   ];
 
   var giphySearchText;
-  var localGiphyCollection = [];
   var counter = 0;
+  var localGiphyCollection = [];
 
+  // Function to add button
   var addButton = (sectionId, category) => {
     var buttonElem = $("<button>");
 
@@ -35,26 +35,32 @@ $(document).ready(function() {
     $(sectionId).append(buttonElem);
   };
 
+  // Function to display Giphy images
   var displayGiphy = giphyObject => {
     if ($("#overwrite-giphy").is(":checked")) {
       clearAllGiphy();
     }
 
+    // The Giphy API reponse object has 25 images, limit it to 10
     for (i = 0; i < 10; i++) {
       var cardDiv = $("<div>");
       var cardHeader = $("<div>");
       var cardImg = $("<img>");
       var cardBodyDiv = $("<div>");
 
+      //Local giphy object to add to the local giphy collection, which will be used in case of no overwrite
       var localGiphyObject = {};
 
+      //The object holds the static & gif images and also the overall index
       localGiphyObject.category = giphySearchText;
       localGiphyObject.still_img_url = giphyObject.data[i].images.fixed_width_small_still.url;
       localGiphyObject.gif_img_url = giphyObject.data[i].images.fixed_width_small.url;
       localGiphyObject.index = counter;
 
+      //Push the object to the collection
       localGiphyCollection.push(localGiphyObject);
 
+      //Create a card element to display the images
       $(cardDiv).addClass("card m-1 border border-info");
       $(cardDiv).attr("style", "width: 18rem; height: 18rem;");
       $(cardDiv).attr("data-index", counter);
@@ -76,9 +82,10 @@ $(document).ready(function() {
       counter++;
     }
 
-    console.log(localGiphyCollection);
+    // console.log(localGiphyCollection);
   };
 
+  //Function to make the Giphy API call
   var getGiphy = queryURL => {
     $.ajax({
       url: queryURL,
@@ -93,6 +100,7 @@ $(document).ready(function() {
       });
   };
 
+  //Function to add new buttons using the form
   $("#add-category").on("click", function(event) {
     event.preventDefault();
     var newCategory = $("#new-category")
@@ -106,6 +114,7 @@ $(document).ready(function() {
     }
   });
 
+  //Callback function for button click, which in turn calls the getGiphy function
   $(document).on("click", ".btn-category", function(event) {
     event.preventDefault();
     giphySearchText = $(this).attr("data-category");
@@ -114,33 +123,39 @@ $(document).ready(function() {
     getGiphy(queryURL);
   });
 
+  //Clear all giphy and reset the counter and collection
   var clearAllGiphy = () => {
     localGiphyCollection = [];
     counter = 0;
     $("#giphy-section").empty();
   };
 
+  //Callback function for Clear All button click
   $("#clear-giphy").on("click", function() {
     clearAllGiphy();
   });
 
-  giphyCategories.forEach(category => {
-    addButton("#btn-section", category);
-  });
-
+  //Callback function for card on click
   $(document).on("click", ".card", function() {
     var index = $(this).attr("data-index");
 
+    //If the image is static, play it
     if ($(this).attr("data-still") === "true") {
       $(this)
         .children("img")
         .attr("src", localGiphyCollection[index].gif_img_url);
       $(this).attr("data-still", "false");
     } else {
+      //If the image is playing, stop it
       $(this)
         .children("img")
         .attr("src", localGiphyCollection[index].still_img_url);
       $(this).attr("data-still", "true");
     }
+  });
+
+  //On page load, the default buttons are added to the page
+  giphyCategories.forEach(category => {
+    addButton("#btn-section", category);
   });
 });
